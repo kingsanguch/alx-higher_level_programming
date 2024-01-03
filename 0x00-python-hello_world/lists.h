4ce1ccf7d1e6a71ffd94b6505833dd8a44658af9
kingsanguch <sangurajohnnie17@gmail.com>
2024-01-03 12:37:05 -0800

    1-run_inline

--- 0x00-python-hello_world/lists.h
+++ 0x00-python-hello_world/lists.h
@@ -1,16 +1,24 @@
 #ifndef LISTS_H
 #define LISTS_H
 
-/* Struct for a node in the list */
-typedef struct Node {
-    int data;
-    struct Node* next;
-} Node;
+**
+ * struct listint_s - singly linked list
+ * @n: integer
+ * @next: points to the next node
+ *
+ * Description: singly linked list node structure
+ * for Holberton project
+ */
 
-/* Function prototypes */
-Node* createNode(int data);
-void insertNode(Node** head, int data);
-void deleteNode(Node** head, int data);
-void printList(Node* head);
+typedef struct listint_s
+{
+        int n;
+        struct listint_s *next;
+} listint_t;
+
+size_t print_listint(const listint_t *h);
+listint_t *add_nodeint(listint_t **head, const int n);
+void free_listint(listint_t *head);
+int check_cycle(listint_t *list);
 
 #endif /* LISTS_H */
