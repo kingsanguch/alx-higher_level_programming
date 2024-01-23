8976c7003b6a758f467fc23fff9a0cca056ffcce
kingsnguch <sangurajohnnie17>
2024-01-23 13:02:20 -0800

    0x05. Python - Exceptions

---
+++ 0x05-python-exceptions/5-raise_exception.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python3
+def raise_exception():
+    raise TypeError("Type exception")
+
+# Uncomment the line below if you want to run the script independently
+# raise_exception()
---
+++ 0x05-python-exceptions/6-raise_exception_msg.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python3
+def raise_exception_msg(message=""):
+    raise NameError(message)
+
+# Uncomment the line below if you want to run the script independently
+# raise_exception_msg("C is fun")
